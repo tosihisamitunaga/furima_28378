5b5183e85801403e3419632a54366b7194e97384
tosihisamitunaga <68527250+tosihisamitunaga@users.noreply.github.com>
2020-08-11 07:39:07 +0900

    ユーザー情報

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
+         validatable :nickname, presence:true,length: { minimum:6}
+         validatable :email, presence: true
+         validatable :email, presence: true, fomat: /\w+@\w+\.{1}[a-zA-Z]{2,}/
+         VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+         validatable :email, {presence: true, ,fomat: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}}
+         validatable :password, presence :true
+         validatable :password, presence :true,length:{minimum:6}
+         validatable :passeord, presence :true, fomat: /(?=.{6,})(?=.*\d+.*)(?=.*[a-zA-Z]+.*).*/
+         validatable :password, confirmation: true
+
 end
