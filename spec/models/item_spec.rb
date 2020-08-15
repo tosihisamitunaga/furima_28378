763b73fa757704eba3fdc4dd5d41d7f8a0dc6521
tosihisamitunaga <68527250+tosihisamitunaga@users.noreply.github.com>
2020-08-15 17:33:10 +0900

    itemモデルの単体テストコード実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create'do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  
+  it'画像が1マイ必須である'do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it'商品名が必須であること'do
+      @item.product_name = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Product name can't be blank")
+    end
+
+    it'商品の説明が必須であること'do
+      @item.coments = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Coments can't be blank")
+    end
+
+    it'カテゴリーの情報が必須であること'do
+      @item.categori = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Categori can't be blank")
+    end
+
+    it'商品の状態について情報が必須であること'do
+    @item.product_status = nil
+    @item.valid?
+      expect(@item.errors.full_messages).to include("Product status can't be blank")
+    end
+
+    it'配送料の負担についての情報が必須であること'do
+    @item.delivery_fee = nil
+    @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
+    end
+
+    it'配送元の地域についての情報が必須であること'do
+    @item.ship_from = nil
+    @item.valid?
+      expect(@item.errors.full_messages).to include("Ship from can't be blank")
+    end
+
+    it'発送までの日数についての情報が必須であること'do
+    @item.shipping_date = nil
+    @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping date can't be blank")
+    end
+
+    it'価格の範囲が¥300~¥9,999,999の間であること'do
+      @item.price = 100
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not included in the list")
+    end
+
+    
+  end
+
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,14 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  #config.include ActionDispatch::TestProcess
+
+  #FactoryBotRails:.class_eval do
+    #include ActionDispatch::TestProcess
+  #end
+
+  #config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
