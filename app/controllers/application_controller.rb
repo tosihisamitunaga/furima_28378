393e2b701d0c97a90ef45f6ae17cd15af6a12343
tosihisamitunaga <68527250+tosihisamitunaga@users.noreply.github.com>
2020-08-16 12:25:18 +0900

    一覧表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,18 @@ class ApplicationController < ActionController::Base
     # before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+    before_action :basic_auth
+
+    private
 
         def configure_permitted_parameters
             devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday])
         end
+
+        def basic_auth
+            authenticate_or_request_with_http_basic do |username, password|
+            username == 'admin' && password == '2222'
+            end
+        end
+        
 end
