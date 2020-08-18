49d5df611e7f68dc30fd375201c73dbf1a6ade63
tosihisamitunaga <68527250+tosihisamitunaga@users.noreply.github.com>
2020-08-18 11:44:17 +0900

    商品詳細機能　修正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
-        before_action :set_item, only: [:show,:edit]
+        before_action :set_item, only: [:show,:edit,:destroy,:update]
 
-        #before_action :authenticate_user!, expect: [:index, :show]
 
     def index
         @items = Item.all.order(created_at: "DESC")
@@ -28,7 +27,6 @@ class ItemsController < ApplicationController
     end
 
     def destroy
-        @item = Item.find(params[:id])
         if @item.destroy
             redirect_to root_path
         else
@@ -37,11 +35,11 @@ class ItemsController < ApplicationController
     end
 
     def update
-        @item = Item.find(params[:id])
         if @item.update(item_params)
             redirect_to root_path
         else
-            render item_path
+            render index
+        end
     end
 
 
